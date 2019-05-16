59c5fa6b01513dd12fb67e7e5b904b724fd3cef7
HISUIE <32667550+hisuie08@users.noreply.github.com>
2019-05-17 06:42:17 +0900

    hmm...

---
+++ main.py
@@ -0,0 +1,18 @@
+import io
+from time import sleep
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+URL = open("url.txt").read()
+path = "lyric.txt"
+options = Options()
+#options.add_argument('--headless')
+driver = webdriver.Chrome(executable_path=r"C:\work\chromedriver.exe", options=options)
+driver.get(URL)
+sleep(1)
+print("ready")
+lyric = driver.find_element_by_css_selector("#lyrics").text
+r = open(path, "w")
+r.write(lyric)
+r.close()
