d79f592a4bf71afed30d434d3ebfcadd94a0777c
HISUIE <32667550+hisuie08@users.noreply.github.com>
2019-05-18 21:59:10 +0900

    support more website

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import io
+import re
 from time import sleep
 
 from selenium import webdriver
@@ -7,12 +8,19 @@ from selenium.webdriver.chrome.options import Options
 URL = open("url.txt").read()
 path = "lyric.txt"
 options = Options()
-#options.add_argument('--headless')
+options.add_argument('--headless')
 driver = webdriver.Chrome(executable_path=r"C:\work\chromedriver.exe", options=options)
 driver.get(URL)
 sleep(1)
 print("ready")
-lyric = driver.find_element_by_css_selector("#lyrics").text
+if re.compile("kasi-time.com").search(URL):
+    lyric = driver.find_element_by_css_selector("#lyrics").text
+elif re.compile("uta-net.com").search(URL):
+    lyric = driver.find_element_by_css_selector("#kashi_area").text
+elif re.compile("utaten.com").search(URL):
+    lyric = driver.find_element_by_css_selector("#contents > main > article > div.lyricBody > div > div.hiragana").text
+elif re.compile("j-lyric.net").search(URL):
+    lyric = driver.find_element_by_css_selector("#Lyric").text
 r = open(path, "w")
 r.write(lyric)
 r.close()
